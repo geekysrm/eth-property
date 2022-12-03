7e0f69e6dcf64a744b8e03e7b62da45447066a6d
geekysrm <soumyacool2012@gmail.com>
2022-12-03 07:47:57 +0530

    temp push protocol

--- app2/src/App.js
+++ app2/src/App.js
@@ -6,8 +6,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import UserProfile from "./pages/UserProfile";
-import AddProperty from './pages/AddProperty';
-import PropertyProfile from './pages/PropertyProfile';
+import AddProperty from "./pages/AddProperty";
+import PropertyProfile from "./pages/PropertyProfile";
+import TestingPush from "./pages/TestingPush";
 
 import Navbar from "./components/Navbar";
 
@@ -26,6 +27,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/push" component={TestingPush} />
           <Route exact path="/user/:userAddress" component={UserProfile} />
           <Route exact path="/add/property" component={AddProperty} />
           <Route exact path="/property/:id" component={PropertyProfile} />
---
+++ app2/src/pages/TestingPush.js
@@ -0,0 +1,4 @@
+export default function TestingPush() {
+  const channelAddress = "0xa715E144881F613d13dC8bff16C1fdfcF0e86aAD";
+  return <div>Jello</div>;
+}
