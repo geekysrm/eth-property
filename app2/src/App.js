bd14b095ec009038935aad3fa9a513c091048e20
K Sai Kishan <sonusaikishan@gmail.com>
2022-12-03 08:05:23 +0530

    Completed Marketplace

--- app2/src/App.js
+++ app2/src/App.js
@@ -8,6 +8,7 @@ import Register from "./pages/Register";
 import UserProfile from "./pages/UserProfile";
 import AddProperty from './pages/AddProperty';
 import PropertyProfile from './pages/PropertyProfile';
+import Marketplace from './pages/Marketplace';
 
 import Navbar from "./components/Navbar";
 
@@ -29,6 +30,7 @@ function App() {
           <Route exact path="/user/:userAddress" component={UserProfile} />
           <Route exact path="/add/property" component={AddProperty} />
           <Route exact path="/property/:id" component={PropertyProfile} />
+          <Route exact path="/marketplace" component={Marketplace} />
         </Switch>
       </Router>
     </ChakraProvider>
---
+++ app2/src/pages/Marketplace.js
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Accordion,
+  AccordionItem,
+  AccordionIcon,
+  AccordionButton,
+  AccordionPanel,
+  Icon,
+} from '@chakra-ui/react';
+
+import CustomMap from '../components/CustomMap';
+
+import isUserRegistered from "../ethereum/isUserRegistered";
+import fetchPropertyDetails from "../ethereum/fetchPropertyDetails";
+import fetchPropertyList from "../ethereum/fetchPropertyList";
+
+export default function Marketplace() {
+  const history = useHistory();
+
+  const [zoomCoord, setZoomCoord] = useState({
+    lat: null,
+    lng: null,
+  });
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      if (typeof window.ethereum !== "undefined") { 
+        const { ethereum } = window;
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+
+        console.log(accounts);
+
+        if(accounts !== 0){
+          const userAddress = accounts[0];
+          console.log(userAddress);
+
+          const response = await isUserRegistered(userAddress);
+          console.log(response);
+
+          if(!response) {
+            history.push(`/`);
+          } else {
+            
+            const propertiesRes = await fetchPropertyList();
+
+            console.log(propertiesRes);
+
+            const propertyDetails = await Promise.all(propertiesRes.map(async p => {
+              const propertyRes = await fetchPropertyDetails(p);
+
+              return {
+                id: p,
+                name: propertyRes[0],
+                dimensions: propertyRes[2],
+                pincode: propertyRes[3],
+                propertyAddress: propertyRes[1],
+                lat: propertyRes[4],
+                lng: propertyRes[5]
+              };
+            }));
+
+            console.log(propertyDetails);
+
+            setProperties(propertyDetails);
+          }
+        }
+      }
+    })();
+  }, []);
+
+  function onHandleAccordionChange(index) {
+    if (index === -1) {
+      setZoomCoord({
+        lat: null,
+        lng: null,
+      });
+    } else {
+      const { lat, lng } = properties[index];
+      setZoomCoord({
+        lat,
+        lng,
+      });
+    }
+  }
+
+  function showPropertyDetails(propertyId) {
+    history.push(`/property/${propertyId}`);
+  }
+
+  return (
+    <Flex
+      style={{
+        marginTop: '20px',
+        marginBottom: '40px',
+        overflowX: 'hidden',
+      }}
+    >
+      <Box width="30%" p={4} m={4} my={0} borderWidth="1px" borderRadius="lg">
+        <Heading size="lg" mb="5">
+          Marketplace:
+        </Heading>
+        {properties.length > 0 ? (
+          <Accordion
+            allowToggle
+            defaultIndex={[0]}
+            onChange={onHandleAccordionChange}
+          >
+            {properties.map(property => (
+              <AccordionItem>
+                <Text>
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      <Heading size="md">{property.name}</Heading>
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </Text>
+                <AccordionPanel pb={4}>
+                  <Text fontSize="lg" display="flex" alignItems="center">
+                    {/* <Icon mr="2" as={AiOutlineHome}></Icon> */}
+                    {`${property.propertyAddress}, ${property.pincode}`}
+                  </Text>
+                  <Text fontSize="lg">
+                    {/* <Icon mr="2" as={BiArea}></Icon> */}
+                    {property.dimensions}
+                  </Text>
+                  <Button
+                    size="sm"
+                    variant="outline"
+                    mt="5"
+                    colorScheme="purple"
+                    onClick={() => showPropertyDetails(property.id)}
+                  >
+                    Show Property
+                  </Button>
+                </AccordionPanel>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        ) : (
+          <Text fontSize="md">No Properties available in marketplace</Text>
+        )}
+      </Box>
+      <Box width="70%" pl={2} pr={4}>
+        <CustomMap
+          properties={properties}
+          zoomLat={zoomCoord.lat}
+          zoomLng={zoomCoord.lng}
+        />
+      </Box>
+    </Flex>
+  );
+}
