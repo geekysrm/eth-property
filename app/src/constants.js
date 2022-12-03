da0480b3aa120acfcd910329ce56ba17851f9db8
geekysrm <soumyacool2012@gmail.com>
2022-12-04 01:03:04 +0530

    final contract deployed

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,6 +1,6 @@
 export const constants = {
-  ethPropertyAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  ethPropertyAddress: "0xF576edCEE24FD1e0795Fb35339bA0A3E3FEFE5bA",
 };
 
-// Goerli test net deployed address: 0x1A1452031653E5cF856401C77Fe94a2F8fd5cd81
+// Goerli test net deployed address: 0xF576edCEE24FD1e0795Fb35339bA0A3E3FEFE5bA
 // How to see smart contract deployed in EtherScan: https://goerli.etherscan.io/
