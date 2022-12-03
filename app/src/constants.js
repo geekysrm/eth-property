f71636964bcecab79d3a8284e7fde517eb0cebbf
K Sai Kishan <sonusaikishan@gmail.com>
2022-12-04 00:02:13 +0530

    trying a fix

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-  ethPropertyAddress: "0x1A1452031653E5cF856401C77Fe94a2F8fd5cd81",
+  ethPropertyAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
 };
 
 // Goerli test net deployed address: 0x1A1452031653E5cF856401C77Fe94a2F8fd5cd81
