17a08ecb46931368830261a6c8ce7209efcefde5
K Sai Kishan <sonusaikishan@gmail.com>
2022-12-04 01:29:06 +0530

    Very imp update

--- app/src/ethereum/fetchUserDetails.js
+++ app/src/ethereum/fetchUserDetails.js
@@ -2,13 +2,19 @@ import EthProperty from "../artifacts/contracts/EthProperty.sol/EthProperty.json
 import { ethers } from "ethers";
 import { constants } from "../constants";
 
+const requestAccount = async () => {
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+}
+
 const fetchUserDetails = async (userAddress) => {
     if (typeof window.ethereum !== "undefined") {
+        await requestAccount();
         const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
         const contract = new ethers.Contract(
           constants.ethPropertyAddress,
           EthProperty.abi,
-          provider
+          signer
         );
         try {
           const data = await contract.fetchUser(userAddress);
