045a94e98f04a86494ea14efe468f111799f3d63
geekysrm <soumyacool2012@gmail.com>
2022-12-03 10:43:42 +0530

    change folder structure

---
+++ app/src/constants.js
@@ -0,0 +1,6 @@
+export const constants = {
+  ethPropertyAddress: "0x2FfE658CbA81FF9eC7B5dA0aC5691f73973b44C4",
+};
+
+// Goerli test net deployed address: 0x2FfE658CbA81FF9eC7B5dA0aC5691f73973b44C4
+// How to see smart contract deployed in EtherScan: https://goerli.etherscan.io/
--- app2/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const constants = {
-  ethPropertyAddress: "0x326CDBc3CA807D087E4059B1c69FDC09ec766314",
-};
-
-// Goerli test net deployed address: 0x326CDBc3CA807D087E4059B1c69FDC09ec766314
-// How to see smart contract deployed in EtherScan: https://goerli.etherscan.io/
